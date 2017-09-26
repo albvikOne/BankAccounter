2f4154340fb48269283dc98bdee5af77434b7159
Victor Albu <albu.victor@outlook.com>
2017-09-26 19:30:21 +0300

    first upload

---
+++ BankAccounter/Controllers/CustomersController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BankAccounter.Models;
+
+namespace BankAccounter.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private BankingSystemV3Entities db = new BankingSystemV3Entities();
+
+
+
+        public ActionResult Index(string searchString)
+        {
+            var customers = from m in db.Customers
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                customers = customers.Where(s => s.customerName.Contains(searchString));
+            }
+
+            return View(customers);
+        }
+
+        //// GET: Customers
+        //public ActionResult Index()
+        //{
+        //    var customers = db.Customers.Include(c => c.Account).Include(c => c.Bank);
+        //    return View(customers.ToList());
+        //}
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            ViewBag.accountID = new SelectList(db.Accounts, "accountID", "accountType");
+            ViewBag.bankID = new SelectList(db.Banks, "bankID", "bankName");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "customerID,customerName,customerSurname,customerAddress,customerPhone,customerAccNo,accountID,bankID")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.accountID = new SelectList(db.Accounts, "accountID", "accountType", customer.accountID);
+            ViewBag.bankID = new SelectList(db.Banks, "bankID", "bankName", customer.bankID);
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.accountID = new SelectList(db.Accounts, "accountID", "accountType", customer.accountID);
+            ViewBag.bankID = new SelectList(db.Banks, "bankID", "bankName", customer.bankID);
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "customerID,customerName,customerSurname,customerAddress,customerPhone,customerAccNo,accountID,bankID")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.accountID = new SelectList(db.Accounts, "accountID", "accountType", customer.accountID);
+            ViewBag.bankID = new SelectList(db.Banks, "bankID", "bankName", customer.bankID);
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
